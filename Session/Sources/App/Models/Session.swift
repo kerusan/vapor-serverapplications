3dac40b8791da895b030fe0d4045612651ac928d
Kjell Nilsson <kerusan@mac.com>
2019-05-16 15:00:32 +0200

    Added gitignore

---
+++ Session/Sources/App/Models/Session.swift
@@ -0,0 +1,60 @@
+import Fluent
+import Vapor
+import Frontbase
+import CFrontbaseSupport
+
+public let expiresInterval = 30.0
+
+/// A single entry of a Session list.
+final class Session: FluentFrontbaseModel {
+    /// The unique identifier for this `Session`.
+    var id: Int?
+    /// A representation describing what this `Session` entails.
+    var representation: String
+    /// A date describing when this `Session` expires.
+    var expires: Date
+    
+    /// Creates a new `Session`.
+    init(id: Int? = nil, exp: Date?, representation: String) {
+        self.id = id
+        self.representation = hashString()
+        if exp! < Date() {
+            self.expires = Date()
+            self.expires.addTimeInterval(expiresInterval)
+        } else {
+            self.expires = exp!
+        }
+    }
+    
+    func updateExpires() {
+        self.expires = Date()
+        self.expires.addTimeInterval(expiresInterval)
+    }
+}
+
+func hashString() -> String {
+    var stringRepresentation = "\(Date().hashValue)"
+    
+    if stringRepresentation.hasPrefix("-") {
+        stringRepresentation.remove(at: stringRepresentation.startIndex)
+    }
+    return stringRepresentation
+}
+
+/// Allows `Session` to be used as a dynamic migration.
+//extension Session: Migration { }
+
+/// Allows `Session` to be encoded to and decoded from HTTP messages.
+extension Session: Content { }
+
+/// Allows `Session` to be used as a dynamic parameter in route definitions.
+extension Session {
+    
+    static func resolveParameter(_ parameter: String, on container: Container) throws -> String {
+        return try next.respond(to: request)
+    }
+    
+    typealias ResolvedParameter = Session
+}
+
+
