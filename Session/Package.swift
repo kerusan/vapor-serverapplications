13e28ee146f99cebcf870f8cd4701812de55eb41
Kjell Nilsson <kerusan@mac.com>
2019-07-11 13:45:56 +0200

    Added FluentFrontbase dependency

--- Session/Package.swift
+++ Session/Package.swift
@@ -2,13 +2,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "Backend",
+    name: "Session",
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on Frontbase.
-        .package(url: "https://github.com/vapor/FluentFrontbase.git", from: "1.0.0")
+        .package(url: "ssh+git://git.oops.se/var/OopsGit/Vapor/FluentFrontbase.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentFrontbase", "Vapor"]),
